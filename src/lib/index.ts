0bf1b5e9371ae4f7a2da160b08909639c332da7d
Kasper <kasperkh.kh@gmail.com>
2025-06-27 01:39:38 +0200

    Add clamp_number

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,9 @@
 export { check_shortcut, check_modifiers, is_mac } from './shortcuts.ts'
 export { auto_snapshot } from './auto-snapshot.ts'
 export { KSelection } from './selection-js.ts'
+
+export function clamp_number(min: number, max: number, value: number) {
+	if (value < min) return min
+	if (value > max) return max
+	return value
+}
