915d886777c1008cb05c8d9bdc878bff022895a9
Kasper <kasperkh.kh@gmail.com>
2025-06-26 08:20:58 +0200

    Create virtual grid demo

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css'
+
+	const { children } = $props()
+</script>
+
+{@render children()}
+
+<style>
+	:global(html) {
+		color-scheme: dark;
+		font-family:
+			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+			'Helvetica Neue', sans-serif;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,68 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { VirtualGrid } from '$lib/virtual-grid.ts'
+
+	function generate_source_items(count: number) {
+		return Array.from({ length: count }, (_, i) => String(Math.random()).slice(2))
+	}
+
+	const virtual_grid = VirtualGrid.create(generate_source_items(50_000), {
+		row_prepare(source_item, index) {
+			return {
+				a: index + 1,
+				b: source_item,
+				c: source_item,
+			}
+		},
+		row_render(element, _item, index) {
+			element.classList.toggle('odd', index % 2 === 0)
+		},
+	})
+	const columns = [
+		{ name: 'a', key: 'a', width: 100 },
+		{ name: 'b', key: 'b', width: 50, is_pct: true },
+		{ name: 'c', key: 'c', width: 50, is_pct: true },
+	]
+	$effect(() => {
+		virtual_grid.set_columns(columns)
+	})
+
+	let item_count = $state(50_000)
+	$effect(() => {
+		virtual_grid.set_source_items(generate_source_items(item_count))
+	})
+</script>
+
+<div class="h-screen max-h-screen gap-2 flex flex-col p-5">
+	<h1 class="text-3xl">Virtual grid</h1>
+	<div>
+		<select class="appearance-auto w-auto bg-gray-700" bind:value={item_count}>
+			{#each [50_000, 500_000] as size}
+				<option value={size}>{Intl.NumberFormat().format(size)} rows</option>
+			{/each}
+		</select>
+	</div>
+	<!-- Virtual grid viewport -->
+	<div class="h-full relative overflow-y-auto bg-black/50 border border-white/20">
+		<!-- Virtual grid content -->
+		<div class="virtual-grid" {@attach virtual_grid.attach()}></div>
+	</div>
+</div>
+
+<style>
+	.virtual-grid :global {
+		.row {
+			height: 24px;
+			width: 100%;
+			position: absolute;
+		}
+		.cell {
+			display: block;
+			height: 100%;
+			position: absolute;
+			height: 24px;
+		}
+		.odd {
+			background-color: hsl(0, 0%, 10%);
+		}
+	}
+</style>
