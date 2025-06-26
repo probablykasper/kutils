cd764b54aa29531fc94f31e7a5a1e747dc8d5fbc
Kasper <kasperkh.kh@gmail.com>
2025-06-26 20:55:00 +0200

    Move global styles to app.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,12 +5,3 @@
 </script>
 
 {@render children()}
-
-<style>
-	:global(html) {
-		color-scheme: dark;
-		font-family:
-			-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
-			'Helvetica Neue', sans-serif;
-	}
-</style>
